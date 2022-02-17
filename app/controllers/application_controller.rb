3d252822201e0f2d3d24f56b01ad5f97094957cd
Kat <bolivarkat@yahoo.com>
2022-02-17 14:25:49 -0600

    added get, delete, and updated houses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,29 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/houses" do
+    house = House.all
+    house.to_json
   end
 
+  patch "/houses/:id" do 
+    house = House.find(params[:id])
+    house.update(
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      areaCode: params[:areaCode],
+      description: params[:description],
+      beds: params[:beds],
+      baths: params[:baths],
+      imageUrl: params[:imageUrl]
+    )
+    house.to_json
+  end
+
+  delete "/houses/:id" do 
+    house = House.find(params[:id])
+    house.destroy
+    house.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+House.create(address: "1703 Paseo Encinal Dr.", city: "Eagle Pass", state: "Texas", areaCode: 78852, description: "Such a Lovely Home", beds: 3, baths: 2, imageUrl: "https://rentportlandhomes.com/wp-content/uploads/2021/04/pexels-photo-186077.jpeg" )
+House.create(address: "1245 Paseo Encinal Dr.", city: "Eagle Pass", state: "Texas", areaCode: 78852, description: "Such a Beautful Home", beds: 6, baths: 1, imageUrl: "https://t4.ftcdn.net/jpg/00/57/68/81/360_F_57688175_PeIsD8zskMwobbM0NIVMIII0nRegWsal.jpg" )
+House.create(address: "1667 Paseo Encinal Dr.", city: "Eagle Pass", state: "Texas", areaCode: 78852, description: "Such a Wonderful Home", beds: 2, baths: 3, imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8WmfmHOMMzQZd9QqnHkJIcm2SYDsOndXeTw&usqp=CAU" )
 
 puts "✅ Done seeding!"
